df14f4ae79a5643d16e83a0d346cc458d36ba98d
guillaumeDiginamic <cguillaume@diginamic-formation.fr>
2023-11-27 14:43:12 +0100

    TP5 Controleur avec 4 verbes

--- src/main/java/fr/diginamic/springdemo/controleurs/VilleControleur.java
+++ src/main/java/fr/diginamic/springdemo/controleurs/VilleControleur.java
@@ -2,27 +2,112 @@ package fr.diginamic.springdemo.controleurs;
 
 import fr.diginamic.springdemo.entites.Ville;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
 @RequestMapping("/villes")
 public class VilleControleur {
+    private List<Ville> villes = new ArrayList<>();
+
+    public VilleControleur() {
+        villes.add(new Ville(1, "Nice", 343000));
+        villes.add(new Ville(2, "Carcassonne", 47800));
+        villes.add(new Ville(3, "Narbonne", 53400));
+        villes.add(new Ville(4, "Lyon", 484000));
+        villes.add(new Ville(5, "Foix", 9700));
+        villes.add(new Ville(6, "Pau", 77200));
+    }
+
     @GetMapping
     public List<Ville> getVilles() {
-        List<Ville> villes = new ArrayList<>();
-        villes.add(new Ville("Nice",343000));
-        villes.add(new Ville("Carcassonne",47800));
-        villes.add(new Ville("Narbonne",53400));
-        villes.add(new Ville("Lyon",484000));
-        villes.add(new Ville("Foix",9700));
-        villes.add(new Ville("Pau",77200));
-
+        // méthode GET qui permet de retrouver toutes les villes
         return villes;
+    }
+
+    @GetMapping("/nom/{nomVille}")
+    public Ville getVille(@PathVariable String nomVille) {
+        // méthode GET qui permet de retrouver une ville à partir de son nom
+        for (Ville v : villes) {
+            if (v.getNom().equals(nomVille)) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Ville getVilleById(@PathVariable int id) {
+        // méthode GET qui permet de retrouver une ville à partir de son id
+        for (Ville v : villes) {
+            if (v.getId() == id) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+    @PutMapping
+    public String insererVille(@RequestBody Ville nvVille) {
+        // méthode PUT qui ne crée une ville que si l’identifiant n'existe pas déjà
+        if (nvVille.getId() == 0) {
+            return "Identifiant incorrect";
+        }
+        for (Ville v : villes) {
+            if (v.getId() == nvVille.getId()) {
+                return "Ville existe déjà";
+            }
+        }
+        villes.add(nvVille);
+        return "Ville modifiée avec succes";
+
+    }
+
+    @PostMapping("/nom/{nomVille}")
+    public String modifierVilleByNom(@PathVariable String nomVille, @RequestBody Ville nvVille) {
+        // méthode POST qui modifie une ville à partir de son nom
+        for (Ville v : villes) {
+            if (v.getNom().equals(nomVille)) {
+                v.setNom(nvVille.getNom());
+                v.setNbHabitants(nvVille.getNbHabitants());
+                return "Ville modifiée avec succes";
+            }
+        }
+        return "Ville " + nomVille + " non trouvée";
+    }
+
+    @PostMapping("/{id}")
+    public String modifierVilleById(@PathVariable int id, @RequestBody Ville nvVille) {
+        // méthode POST qui modifie une ville à partir de son id
+        if (id <= 0) {
+            return "Identifiant incorrect";
+        }
+        for (Ville v : villes) {
+            System.out.println("ici");
+            if (v.getId() == id) {
+                v.setNom(nvVille.getNom());
+                v.setNbHabitants(nvVille.getNbHabitants());
+                return "Ville modifiée avec succes";
+            }
+        }
+        return "Ville id=" + id + " non trouvée";
+    }
+
+    @DeleteMapping("delete/{id}")
+    public String deleteVilleById(@PathVariable int id, @RequestBody Ville nvVille) {
+        // méthode DELETE qui supprime une ville à partir de son id
+        Iterator<Ville> iter = villes.iterator();
+        while (iter.hasNext()) {
+            Ville ville = iter.next();
+            if (ville.getId() == id) {
+                iter.remove();
+                return "Ville supprimée avec succes";
+            }
+        }
+        return "Ville id=" + id + " non trouvée";
     }
 }
--- src/main/java/fr/diginamic/springdemo/entites/Ville.java
+++ src/main/java/fr/diginamic/springdemo/entites/Ville.java
@@ -1,19 +1,47 @@
 package fr.diginamic.springdemo.entites;
 
+import java.util.Objects;
+
 public class Ville {
+    // Ajouter Id
+    private int id;
     private String nom;
     private int nbHabitants;
 
-    public Ville(String nom, int nbHabitants) {
+    public Ville() {
+    }
+
+    public Ville(int id, String nom, int nbHabitants) {
+        this.id = id;
         this.nom = nom;
         this.nbHabitants = nbHabitants;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ville ville = (Ville) o;
+        return id ==ville.id && nbHabitants == ville.nbHabitants && Objects.equals(nom, ville.nom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, nbHabitants);
+    }
+
     @Override
     public String toString() {
         return nom + " ("+nbHabitants+")";
     }
 
-    public String getNom() {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }public String getNom() {
         return nom;
     }
 
